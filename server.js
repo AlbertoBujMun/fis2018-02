76572ddddcf0a1181a4b08175237796b43b18251
rafavermo@gmail.com <rafael.vergara@sngular.team>
2019-01-08 22:17:44 +0100

    Solved error to connect database for remove project

--- server.js
+++ server.js
@@ -13,19 +13,19 @@ const PROYECTS_APP_DIR = "/dist/proyects-app";
 var BASE_API_PATH = "/api/v1";
 
 passport.use(
-  new LocalAPIKey((apikey, done) => {
-    ApiKey.findOne({ apikey: apikey }, (err, user) => {
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        return done(null, false, { message: "Unknown apikey " + apikey });
-      } else {
-        console.log("Logged as: " + user.user);
-        return done(null, user);
-      }
-    });
-  })
+    new LocalAPIKey((apikey, done) => {
+        ApiKey.findOne({ apikey: apikey }, (err, user) => {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                return done(null, false, { message: "Unknown apikey " + apikey });
+            } else {
+                console.log("Logged as: " + user.user);
+                return done(null, user);
+            }
+        });
+    })
 );
 
 var app = express();
@@ -34,113 +34,113 @@ app.use(passport.initialize());
 app.use(cors());
 app.use(express.static(path.join(__dirname, PROYECTS_APP_DIR)));
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, PROYECTS_APP_DIR, "/index.html"));
+    res.sendFile(path.join(__dirname, PROYECTS_APP_DIR, "/index.html"));
 });
 
 app.get(
-  BASE_API_PATH + "/proyects",
-  passport.authenticate("localapikey", { session: false }),
-  (req, res) => {
-    Proyect.find((err, proyects) => {
-      if (err) {
-        console.error("Error accessing database");
-        res.sendStatus(500);
-      } else {
-        res.send(
-          proyects.map(proyect => {
-            return proyect.cleanup();
-          })
-        );
-      }
-    });
-  }
+    BASE_API_PATH + "/proyects",
+    passport.authenticate("localapikey", { session: false }),
+    (req, res) => {
+        Proyect.find((err, proyects) => {
+            if (err) {
+                console.error("Error accessing database");
+                res.sendStatus(500);
+            } else {
+                res.send(
+                    proyects.map(proyect => {
+                        return proyect.cleanup();
+                    })
+                );
+            }
+        });
+    }
 );
 
 app.post(
-  BASE_API_PATH + "/proyects",
-  passport.authenticate("localapikey", { session: false }),
-  (req, res) => {
-    // Create a new proyect
-    console.log(Date() + " - POST /proyects");
-    var proyect = req.body;
-    proyect.id =
-      new Date().toISOString().substr(0, 9).replace("-","") +
-      Math.floor(Math.random() * (1000 - 1 + 1)) +
-      1;
-    Proyect.create(proyect, err => {
-      if (err) {
-        console.error(err);
-        res.sendStatus(500);
-      } else {
-        res.sendStatus(201);
-      }
-    });
-  }
+    BASE_API_PATH + "/proyects",
+    passport.authenticate("localapikey", { session: false }),
+    (req, res) => {
+        // Create a new proyect
+        console.log(Date() + " - POST /proyects");
+        var proyect = req.body;
+        proyect.id =
+            new Date().toISOString().substr(0, 9).replace("-", "") +
+            Math.floor(Math.random() * (1000 - 1 + 1)) +
+            1;
+        Proyect.create(proyect, err => {
+            if (err) {
+                console.error(err);
+                res.sendStatus(500);
+            } else {
+                res.sendStatus(201);
+            }
+        });
+    }
 );
 
 app.put(
-  BASE_API_PATH + "/proyects",
-  passport.authenticate("localapikey", { session: false }),
-  (req, res) => {
-    console.log(Date() + " - PUT /proyects");
-    var proyects = req.body;
-    Proyect.update(proyect, err => {
-      if (err) {
-        console.error(err);
-        res.sendStatus(500);
-      } else {
-        res.sendStatus(201);
-      }
-    });
-  }
+    BASE_API_PATH + "/proyects",
+    passport.authenticate("localapikey", { session: false }),
+    (req, res) => {
+        console.log(Date() + " - PUT /proyects");
+        var proyects = req.body;
+        Proyect.update(proyect, err => {
+            if (err) {
+                console.error(err);
+                res.sendStatus(500);
+            } else {
+                res.sendStatus(201);
+            }
+        });
+    }
 );
 
 app.delete(
-  BASE_API_PATH + "/proyects",
-  passport.authenticate("localapikey", { session: false }),
-  (req, res) => {
-    // Remove all proyects
-    console.log(Date() + " - DELETE /proyects");
-    res.sendStatus(403)
-  }
+    BASE_API_PATH + "/proyects",
+    passport.authenticate("localapikey", { session: false }),
+    (req, res) => {
+        // Remove all proyects
+        console.log(Date() + " - DELETE /proyects");
+        res.sendStatus(403)
+    }
 );
 
 app.get(
-  BASE_API_PATH + "/proyects/:id",
-  passport.authenticate("localapikey", { session: false }),
-  (req, res) => {
-    var id = req.params.id;
-    console.log(Date() + " - GET /proyects/" + id);
-    // Get a single proyect
-    Proyect.find({ id: id }, (err, proyects) => {
-      if (err) {
-        console.error("Error accessing database");
-        res.sendStatus(500);
-      } else {
-        res.send(
-          proyects.map(proyect => {
-            return proyect.cleanup();
-          })
-        );
-      }
-    });
-  }
+    BASE_API_PATH + "/proyects/:id",
+    passport.authenticate("localapikey", { session: false }),
+    (req, res) => {
+        var id = req.params.id;
+        console.log(Date() + " - GET /proyects/" + id);
+        // Get a single proyect
+        Proyect.find({ id: id }, (err, proyects) => {
+            if (err) {
+                console.error("Error accessing database");
+                res.sendStatus(500);
+            } else {
+                res.send(
+                    proyects.map(proyect => {
+                        return proyect.cleanup();
+                    })
+                );
+            }
+        });
+    }
 );
 
 app.get(BASE_API_PATH + "/proyects/:id", (req, res) => {
     // Get a single proyect
     var id = req.params.id;
-    console.log(Date()+" - GET /proyects/"+id);
+    console.log(Date() + " - GET /proyects/" + id);
 
-    db.find({"id": id},(err,proyects)=>{
-        if(err){
+    Proyect.find({ "id": id }, (err, proyects) => {
+        if (err) {
             console.error("Error accesing DB");
             res.sendStatus(500);
-        }else{
-            if(proyects.length>1){
+        } else {
+            if (proyects.length > 1) {
                 console.warn("Incosistent DB: duplicated id");
             }
-            res.send(proyects.map((proyect)=>{
+            res.send(proyects.map((proyect) => {
                 delete proyect._id;
                 return proyect;
             })[0]);
@@ -152,16 +152,16 @@ app.get(BASE_API_PATH + "/proyects/:id", (req, res) => {
 app.delete(BASE_API_PATH + "/proyects/:id", (req, res) => {
     // Delete a single proyect
     var id = req.params.id;
-    console.log(Date()+" - DELETE /proyects/"+id);
+    console.log(Date() + " - DELETE /proyects/" + id);
 
-    db.remove({"id": id},{},(err,numRemoved)=>{
-        if(err){
+    Proyect.remove({ "id": id }, {}, (err, numRemoved) => {
+        if (err) {
             console.error("Error accesing DB");
             res.sendStatus(500);
-        }else{
-            if(numRemoved>1){
+        } else {
+            if (numRemoved > 1) {
                 console.warn("Incosistent DB: duplicated id");
-            }else if(numRemoved == 0) {
+            } else if (numRemoved == 0) {
                 res.sendStatus(404);
             } else {
                 res.sendStatus(200);
@@ -172,16 +172,16 @@ app.delete(BASE_API_PATH + "/proyects/:id", (req, res) => {
 app.delete(BASE_API_PATH + "/proyects/:id", (req, res) => {
     // Delete a single proyect
     var id = req.params.id;
-    console.log(Date()+" - DELETE /proyects/"+id);
+    console.log(Date() + " - DELETE /proyects/" + id);
 
-    db.remove({"id": id},{},(err,numRemoved)=>{
-        if(err){
+    Proyect.remove({ "id": id }, {}, (err, numRemoved) => {
+        if (err) {
             console.error("Error accesing DB");
             res.sendStatus(500);
-        }else{
-            if(numRemoved>1){
+        } else {
+            if (numRemoved > 1) {
                 console.warn("Incosistent DB: duplicated id");
-            }else if(numRemoved == 0) {
+            } else if (numRemoved == 0) {
                 res.sendStatus(404);
             } else {
                 res.sendStatus(200);
@@ -194,21 +194,21 @@ app.put(BASE_API_PATH + "/proyects/:id", (req, res) => {
     // Update proyect
     var id = req.params.id;
     var updatedProyect = req.body;
-    console.log(Date()+" - PUT /proyects/"+id);
+    console.log(Date() + " - PUT /proyects/" + id);
 
-    if(id != updatedProyect.id){
+    if (id != updatedProyect.id) {
         res.sendStatus(409);
         return;
     }
 
-    db.update({"id": id},updatedProyect,(err,numUpdated)=>{
-        if(err){
+    Proyect.update({ "id": id }, updatedProyect, (err, numUpdated) => {
+        if (err) {
             console.error("Error accesing DB");
             res.sendStatus(500);
-        }else{
-            if(numUpdated>1){
+        } else {
+            if (numUpdated > 1) {
                 console.warn("Incosistent DB: duplicated id");
-            }else if(numUpdated == 0) {
+            } else if (numUpdated == 0) {
                 res.sendStatus(404);
             } else {
                 res.sendStatus(200);
